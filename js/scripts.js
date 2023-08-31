f30dd1fc84bc0781d118de40ef54a937b426882f
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2023-08-31 17:35:04 -0400

    Created base pokemon list
    created base pokemon list variable and added 3 new objects to the variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
-alert('Hello World');
-
-let FavoriteFood = 'Baked Chicken';
-document.write(FavoriteFood);
+let pokemonlist = [];
+pokemonlist = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        weight: 6.9,
+        type: ['grass', 'poison'],
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        weight: 8.5,
+        type: ['fire'],
+    },
+    {
+        name: 'Squirtle',
+        height: 0.5,
+        weight: 9,
+        type: ['water'],
+    }
+]
