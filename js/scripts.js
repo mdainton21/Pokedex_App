41f0c7dbe19f1725727fc6435ed1561c8a73ee23
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2023-09-03 20:57:56 -0400

    Created loop to write to main page
    Created a loop that writes pokemon name/height/weight to the main page. Also added a conditional that writes extra if height or weight are past a certain threshold.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,21 @@ pokemonlist = [
         type: ['grass', 'poison'],
     },
     {
+        name: 'Ivysaur',
+        height: 1,
+        weight: 13,
+        type: ['grass', 'poison'],
+    },
+    {
         name: 'Charmander',
         height: 0.6,
         weight: 8.5,
+        type: ['fire'],
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        weight: 19,
         type: ['fire'],
     },
     {
@@ -17,5 +29,25 @@ pokemonlist = [
         height: 0.5,
         weight: 9,
         type: ['water'],
+    },
+    {
+        name: 'Wartortle',
+        height: 1,
+        weight: 22.5,
+        type: ['water'],
     }
 ]
+
+// Loop to print pokemon name/height
+for (let i = 0; i < pokemonlist.length; i++) {
+    document.write(pokemonlist[i].name);
+    document.write(' (height: ' + pokemonlist[i].height + 'm)');
+    document.write(' (weight: ' + pokemonlist[i].weight + 'kg)');
+    // Conditional to check height and weight
+    if (pokemonlist[i].height > 1) {
+        document.write(' - That is pretty tall!');
+    } else if (pokemonlist[i].weight > 20) {
+        document.write(' - That is quite heavy!')
+    }
+    document.write('<br><br>')
+}
