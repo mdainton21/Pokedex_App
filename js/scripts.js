c3c0b55fee56ec6f7a5bd4a8ef13046bb8164fc1
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2023-09-09 18:25:09 -0400

    Added IIFE for main list
    Wrapped the main pokemonlist array in an IIFE, and made sure all references to that array were reformatted for the new IIFE.

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,62 @@
-let pokemonlist = [];
-pokemonlist = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        weight: 6.9,
-        type: ['grass', 'poison'],
-    },
-    {
-        name: 'Ivysaur',
-        height: 1,
-        weight: 13,
-        type: ['grass', 'poison'],
-    },
-    {
-        name: 'Charmander',
-        height: 0.6,
-        weight: 8.5,
-        type: ['fire'],
-    },
-    {
-        name: 'Charmeleon',
-        height: 1.1,
-        weight: 19,
-        type: ['fire'],
-    },
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        weight: 9,
-        type: ['water'],
-    },
-    {
-        name: 'Wartortle',
-        height: 1,
-        weight: 22.5,
-        type: ['water'],
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            weight: 6.9,
+            type: ['grass', 'poison'],
+        },
+        {
+            name: 'Ivysaur',
+            height: 1,
+            weight: 13,
+            type: ['grass', 'poison'],
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            weight: 8.5,
+            type: ['fire'],
+        },
+        {
+            name: 'Charmeleon',
+            height: 1.1,
+            weight: 19,
+            type: ['fire'],
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            weight: 9,
+            type: ['water'],
+        },
+        {
+            name: 'Wartortle',
+            height: 1,
+            weight: 22.5,
+            type: ['water'],
+        }
+    ]
+
+    function getAll() {
+        return pokemonList;
     }
-]
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add
+    };
+})();
+
+// IIFE check
+console.log(pokemonRepository.getAll());
+pokemonRepository.add({ name: 'Blastoise' });
 
 // Loop to print pokemon name/height
-pokemonlist.forEach(function (item) {
+pokemonRepository.getAll().forEach(function (item) {
     document.write(item.name);
     document.write(' (height: ' + item.height + 'm)');
     document.write(' (weight: ' + item.weight + 'kg)');
