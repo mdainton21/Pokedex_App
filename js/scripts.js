37c25ad89e8086c56ad8e923f95ae918c1edb282
mdainton21 <142181864+mdainton21@users.noreply.github.com>
2023-09-28 23:43:30 -0400

    Introduced Bootstrap formatting
    Added Bootstrap to the app and began introducing a Nav bar and adding correct class designations for the future.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,34 @@
 // IIFE wrapping
 let pokemonRepository = (function () {
     let pokemonList = [];
-    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+    // let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
+    
+    // setup API fetches for each Gen of pokemon
+    let apiUrl = [
+        { url: 'https://pokeapi.co/api/v2/pokemon/?limit=151', gen: 'I', offset: 0 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=151&limit=100', gen: 'II', offset: 151 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=251&limit=135', gen: 'III', offset: 251 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=386&limit=107', gen: 'IV', offset: 386 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=493&limit=156', gen: 'V', offset: 493 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=649&limit=72', gen: 'VI', offset: 649 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=721&limit=88', gen: 'VII', offset: 721 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=809&limit=96', gen: 'VIII', offset: 809 },
+        { url: 'https://pokeapi.co/api/v2/pokemon/?offset=905&limit=105', gen: 'IX', offset: 905 }
+    ];
+
+    // reset previous data
+    let currentApi = apiUrl[0];
+
+
+
+
 
     function add(pokemon) {
         // data check before push
         if (
             typeof pokemon === "object" &&
             "name" in pokemon
+
         ) {
             pokemonList.push(pokemon);
         } else {
@@ -22,8 +43,9 @@ let pokemonRepository = (function () {
     function addListItem(pokemon) {
         let pokemonQuery = document.querySelector(".pokemon-list");
         let pokemonItem = document.createElement("li");
+        pokemonItem.classList.add("list-group-item");
         let button = document.createElement("button");
-        button.innerText = pokemon.name;
+        button.innerText = pokemon.id + ". " + pokemon.name;
         button.classList.add("button-class");
 
         // Event listener. Activates the showDetails function upon pokemon name click
@@ -38,16 +60,18 @@ let pokemonRepository = (function () {
 
     // Function to fetch PokeAPI and save to list
     function loadList() {
-        return fetch(apiUrl).then(function (response) {
+        return fetch(currentApi.url).then(function (response) {
             return response.json();
         }).then(function (json) {
+            let pokeId = currentApi.offset + 1;
             json.results.forEach(function (item) {
                 let pokemon = {
                     name: item.name,
-                    id: item.id,
-                    detailsUrl: item.url
+                    detailsUrl: item.url,
+                    id: pokeId
                 };
                 add(pokemon);
+                pokeId = pokeId + 1
             });
         }).catch(function (e) {
             console.error(e);
@@ -60,8 +84,14 @@ let pokemonRepository = (function () {
             return response.json();
         }).then(function (details) {
             // Load specific details for each pokemon
+            
+            //Images
             item.imageUrl = details.sprites.other["official-artwork"].front_default;
+            item.spriteUrl = details.sprites.front_default;
+            
+            //Other Details
             item.id = details.id;
+            
             item.height = details.height;
             item.weight = details.weight;
             item.types = [];
